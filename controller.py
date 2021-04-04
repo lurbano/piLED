4d10542cf7c96951271bf7a98a7f8048aa715843
lurbano <lurbano@earthsciweb.org>
2021-04-03 22:11:33 -0500

    test

--- controller.py
+++ controller.py
@@ -1,10 +1,15 @@
 import subprocess
+import time
 
 p = subprocess.Popen(["sudo", "python3", "runLEDs.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE,  universal_newlines=True)
 
-q = input("?>")
+time.sleep(1)
+p.stdin.write("g")
+print(done)
 
-if q == "green":
-    p.stdin.write("g")
-    #print(b'g', file=p.stdin)
-    #p.stdin.flush()
+# q = input("?>")
+#
+# if q == "green":
+#     p.stdin.write("g")
+#     #print(b'g', file=p.stdin)
+#     #p.stdin.flush()
