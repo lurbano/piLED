d49444fde6a34e6fd84745a13d06c2fd0fd255e2
lurbano <lurbano@earthsciweb.org>
2021-04-03 22:49:43 -0500

    fix

--- pexp.py
+++ pexp.py
@@ -3,6 +3,6 @@ import sys
 
 child = pexpect.spawn('sudo python3 runLEDs.py', encoding='utf-8')
 child.expect("Ready")
-child.sendline('{"info": "green"}')
+child.sendline('{"what": "green"}')
 child.logfile = sys.stdout
 print('done')
