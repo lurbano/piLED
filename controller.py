4afdde537e1cf4cd5f986a2c039f29a59ee30579
lurbano <lurbano@earthsciweb.org>
2021-04-03 21:56:11 -0500

    test

--- controller.py
+++ controller.py
@@ -5,5 +5,6 @@ p = subprocess.Popen(["sudo", "python3", "runLEDs.py"], stdin=subprocess.PIPE, s
 q = input("?>")
 
 if q == "green":
-    print(b'g', file=p.stdin)
+    p.stdin.write("g\n")
+    #print(b'g', file=p.stdin)
     #p.stdin.flush()
