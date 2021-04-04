1ce9b782c3e30750c8247a372622e57644fbdee8
lurbano <lurbano@earthsciweb.org>
2021-04-04 02:29:30 -0500

    test

--- webServer/server.py
+++ webServer/server.py
@@ -33,12 +33,12 @@ wsCast = wsBroadcasterU()
 try:
 	nPix = 20
 	ledPin = board.D18
-
+	print("nPix", nPix)
 	# get number of pixels from the command line
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-n", "--nPix", help = "Number of pixels")
 	args = parser.parse_args()
-
+	print(args)
 	if args.nPix:
 		try:
 			nPix = int(args.nPix)
